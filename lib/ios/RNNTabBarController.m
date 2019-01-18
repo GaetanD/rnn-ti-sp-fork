9e92fc552d83da0c4d50289f2369a608f224ee24
Tano Interactive <hosting@tano-interactive.com>
2019-01-18 11:50:44 +0100

    Placeholder tab
    The 'Menu' tab is only use to toggle the drawer menu. The `MenuPlaceholderContainer` component should not be displayed. But we still listen for events with `registerBottomTabSelectedListener` to know when the tab is pressed.
    
    The 'Menu' tab position is assumed to always be #4.

--- lib/ios/RNNTabBarController.m
+++ lib/ios/RNNTabBarController.m
@@ -50,4 +50,16 @@
 	_currentTabIndex = tabBarController.selectedIndex;
 }
 
+
+// Fork: menu tab is only used as a placeholder, no VC should be mounted
+- (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController {
+	 NSUInteger selectedIndex = [tabBarController.viewControllers indexOfObject:viewController];
+	if (selectedIndex == 4) {
+		// We still send the event so we can use it on the JS side
+		[_eventEmitter sendBottomTabSelected:@(4) unselected:@(_currentTabIndex)];
+		return NO;
+	}
+	return YES;
+}
+
 @end
